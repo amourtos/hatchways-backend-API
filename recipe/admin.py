26dfd5c7bb30eb9e0a2058dba221b89acfd1c890
amourtos <amourtos@gmail.com>
2021-11-06 14:57:08 -0400

    admin panel set up for recipe instructions and recipe ingredients to appear as listed properties

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from recipe.models import Recipe
+from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
+
 # Register your models here.
-admin.site.register(Recipe)
+
+
+class RecipeAdmin(admin.ModelAdmin, DynamicArrayMixin):
+    pass
+
+
+admin.site.register(Recipe, RecipeAdmin)
